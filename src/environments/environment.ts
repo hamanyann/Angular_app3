c1caf6d9743c65d7ad95e3226bb6478d9d8a5fc3
hama <75150231+hamanyann@users.noreply.github.com>
2024-09-04 22:26:12 +0900

    a

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,6 @@
-// src/environments/environment.ts
 export const environment = {
     production: false,
-    pusherKey: 'your_pusher_key',
-    pusherCluster: 'your_pusher_cluster'
+    pusherKey: process.env["77a3b7d204aee6311cbd"] || '',
+    pusherCluster: process.env['ap3'] || '',
   };
   
